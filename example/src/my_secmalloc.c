f78dc79fa3bec952470231ca76796fcc7a0d2331
darius.raffaud <darius.raffaud@ecole2600.com>
2024-06-02 15:38:56 +0200

    save

--- example/src/my_secmalloc.c
+++ example/src/my_secmalloc.c
@@ -162,3 +162,26 @@ void* my_realloc(void* ptr, size_t size) {
     // Return a pointer to the start of the reallocated block
     return new_ptr;
 }
+
+
+#ifdef DYNAMIC
+void* malloc(size_t size)
+{
+    return my_malloc(size);
+}
+void    free(void* ptr)
+{
+    my_free(ptr);
+}
+void* calloc(size_t nmemb, size_t size)
+{
+    return my_calloc(nmemb, size);
+}
+
+void* realloc(void* ptr, size_t size)
+{
+    return my_realloc(ptr, size);
+
+}
+
+#endif
